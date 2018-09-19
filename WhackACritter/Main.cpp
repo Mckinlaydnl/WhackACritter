18eb6f310acd0a920d734879f96c74fa208d9a1f
Mckinlaydnl <43002081+Mckinlaydnl@users.noreply.github.com>
2018-09-19 13:24:31 +0100

    Added sfml debugs

--- WhackACritter/Main.cpp
+++ WhackACritter/Main.cpp
@@ -1,22 +1,43 @@
 #include <SFML/Graphics.hpp>
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	// Make a variable called gameWindow of type RenderWindow
+	sf::RenderWindow gameWindow;
+	gameWindow.create(sf::VideoMode::getDesktopMode(), "WhackACritter",
+		sf::Style::Titlebar | sf::Style::Close);
+	sf::Clock gameClock;
 
-	while (window.isOpen())
+
+	while (gameWindow.isOpen())
 	{
+
+		//----------------------------------------------------
+		//Input
+		//----------------------------------------------------
 		sf::Event event;
-		while (window.pollEvent(event))
+		while (gameWindow.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
-				window.close();
-		}
-		window.clear();
-		window.draw(shape);
-		window.display();
-	}
+			{
+				gameWindow.close();
+			}//End if (closed)
+		}//end event polling loop
+
+		//----------------------------------------------------
+		//End input
+		//----------------------------------------------------
+		sf::Time frameTime = gameClock.restart();
+
+		//clear the window to a single colour
+		gameWindow.clear();
+
+		//draw everything
+
+		//display the windows contents to the screen
+		gameWindow.display();
+
+		//end draw
+	}//end of the game while loop
 
 	return 0;
 }
